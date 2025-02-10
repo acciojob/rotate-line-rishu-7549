7333bbb9e4900c687c2a0df02390b66db2b97a80
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-10 22:24:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+    let line = document.getElementById("line");
+    let angle = 0;
+
+    setInterval(() => {
+        angle += 2; // Increase angle by 2 degrees
+        line.style.transform = `rotate(${angle}deg)`;
+    }, 20); // Update every 20 milliseconds
+});
